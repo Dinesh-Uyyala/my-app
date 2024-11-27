112a565395979482f681a9f0cc43bc93a9e3d434
Dinesh-Uyyala <dineshuyyala1@gmail.com>
2024-11-27 10:49:56 +0530

    login page view, reactive forms implemented and api integration added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { LoginService } from '../login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  public loginForm:FormGroup=new FormGroup({
+    email:new FormControl(),
+    password:new FormControl(),
+  })
+
+  constructor(private _loginService:LoginService, private routes:Router){}
+
+  login(){
+    console.log(this.loginForm);
+    this._loginService.login(this.loginForm.value).subscribe((data:any)=>{
+      alert("Login Success!!");
+      this.routes.navigate(['/dashboard']);
+    },(err:any)=>{
+      alert("Invalid Credentials")
+    }
+  )
+  }
 
 }
